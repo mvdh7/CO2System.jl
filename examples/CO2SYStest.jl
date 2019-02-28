7d30d7d73d59cda34da487cd2e6c797d11f4fda8
Matthew Humphreys <mvdh7@users.noreply.github.com>
2019-02-28 12:38:09 +0000

    Move to examples folder

--- examples/CO2SYStest.jl
+++ examples/CO2SYStest.jl
@@ -1,6 +1,6 @@
-module co
+module CO2
 
-include("src/CO2SYS.jl")
+include("../src/CO2SYS.jl")
 
 data, heads, niceheads = CO2SYS(
     2300, 2250, 1, 2, 35, 10, 10, 5, 5, 10, 1, 3, 10, 4)
@@ -10,4 +10,4 @@ test = CO2SYS(2400,2000:10:2400,1,2,35,0,25,4200,0,15,1,1,4,1)[1]
 println(test[:,3])
 println(size(test))
 
-end # module co
+end # module CO2
